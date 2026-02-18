85218a8dda9ffb3f4930577b41afef3683c29966
Janisse-rc <alorc2707@gmail.com>
2026-02-18 03:53:33 -0500

    calling functions

---
+++ project/scripts/expect-form.js
@@ -0,0 +1,49 @@
+const input = document.querySelector('#favchap');
+const button = document.querySelector('#addChapter');
+const list = document.querySelector('#list');    //you need to fill in the blank to reference the HTML element that is a unordered list element.
+
+
+//Handling DOM Events
+button.addEventListener('click', function () {
+    // Code to execute when the button is clicked
+
+    const chapter = input.value.trim();
+
+    if (chapter !== '') {
+        //buttom
+
+        //Create list item
+        const li = document.createElement('li');
+        //We identified this variable as li(that is next to const), howerver, that was just
+        // for simplicity. The variable identifier/name did not have
+        // to be named the same as the element being created.
+
+        const span = document.createElement('span');
+        span.textContent = chapter
+
+        //Delete button
+        const deleteButton = document.createElement('button');
+
+        //Populate the button textContent with a X
+        deleteButton.textContent = '✖️';
+
+        //Add delete functionality
+        deleteButton.addEventListener('click', function () {
+            li.remove();
+            input.focus();
+        });
+
+        li.append(span);
+        //Append the li element variable with the delete button.
+        li.append(deleteButton);
+
+        //Append li element variable to the unordered list in your HTML.
+        list.append(li);
+
+        //Clear input
+        input.value = '';
+    }
+    //Whether or not a list item was created, the focus (active cursor) should be sent to the input element.
+    input.focus();
+});
+
